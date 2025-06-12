a959f77dab0d210cd3678e84c59b066a12cbd0d4
Lucas Emanuel Diaz <Lucas_E_Diaz@hotmail.com>
2025-06-12 19:32:06 -0300

    añado post y delete

--- index.js
+++ index.js
@@ -1,27 +1,64 @@
 // destructury: del comando inicial extraigo la 3era y 4ta palabra
 const [, , method, resource] = process.argv;
 
-// funcion reutilizable para obtener el json
-async function fetchData(path) {
-  // Hacemos la petición con fetch
+// funcion para obtener datos
+// path => [3] palabra del comando
+async function getData(path) {
   const response = await fetch("https://fakestoreapi.com/" + path);
-  return await response.json();
+  const data = await response.json();
+  console.log(data);
+}
+
+// funcion para crear un dato en el servidor
+// path => [3] palabra del comando
+// body => objeto conformado por:
+//   title => [4] palabra del comando
+//   price => [5] palabra del comando
+//   category => [6] palabra del comando
+async function postData(path, body) {
+  const response = await fetch("https://fakestoreapi.com/" + path, {
+    method: "POST",
+    body: body
+  });
+
+  const data = await response.json();
+  console.log(data);
+}
+
+// funcion para eliminar un dato en el servidor
+// path => [3] palabra del comando
+// id => [4] palabra del comando
+async function deleteData(path, id) {
+  const response = await fetch("https://fakestoreapi.com/" + path + "/" + id, {
+    method: "DELETE",
+  });
+
+  const data = await response.json();
+  console.log(data);
 }
 
 if (method && resource) {
-  // .toUpperCase() convierte todo a mayuscula
-  // "get" => "GET", "poST" => "POST"
-  if (method.toUpperCase() !== "GET") {
-    console.error("solo GET esta soportado");
+  if (method.toUpperCase() === "GET") {
+    getData(resource);
   }
 
-  // usamos la funcion de arriba pasandole resource como parametro
-  fetchData(resource)
-    .then(data => {
-      console.log([...data]);
-    })
-    .catch(err => {
-      console.error('Error:', err.message);
-    })
-    .finally(() => process.exit());
+  if (method.toUpperCase() === "POST") {
+    const title = process.argv[4];
+    const price = process.argv[5];
+    const category = process.argv[6];
+
+    const body = {
+      title,
+      price,
+      category
+    };
+
+    postData(resource, body);
+  }
+
+  if (method.toUpperCase() === "DELETE") {
+    const id = process.argv[4];
+
+    deleteData(resource, id);
+  }
 }
